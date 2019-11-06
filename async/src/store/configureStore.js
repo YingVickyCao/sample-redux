9ec9cbe93a49c3fb5b2fd5a2c10230fda46d9b13
Vicky <ying.vicky.cao@outlook.com>
2019-11-06 17:23:16 +0800

    Update configureStore.js

--- async/src/store/configureStore.js
+++ async/src/store/configureStore.js
@@ -3,10 +3,9 @@ import thunkMiddleware from "redux-thunk";
 import { createLogger } from "redux-logger";
 import rootReducer from "./reducers";
 
-const loggerMiddleware = createLogger();
-
 export default function configureStore(preloadedState) {
   if (process.env.NODE_ENV !== "production") {
+    const loggerMiddleware = createLogger();
     return createStore(
       rootReducer,
       preloadedState,
