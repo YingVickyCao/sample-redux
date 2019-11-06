09756cfd6ccc1e6bf33f0db3fc446f1a53957ed9
Vicky <ying.vicky.cao@outlook.com>
2019-11-06 13:41:53 +0800

    Update index.js

--- Counter/src/index.js
+++ Counter/src/index.js
@@ -3,12 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-import { createStore } from "redux";
-import reducer from "./reducers/counter";
-
-let store = createStore(reducer);
-export default store;
-
 ReactDOM.render(<App />, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
