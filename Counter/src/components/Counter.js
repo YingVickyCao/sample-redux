0b390c0a5867a24241a744b8acbfe983e64b899d
Vicky <ying.vicky.cao@outlook.com>
2019-11-06 14:13:52 +0800

    rerfactor

--- Counter/src/components/Counter.js
+++ Counter/src/components/Counter.js
@@ -1,44 +1,44 @@
-import React, { Component } from 'react';
-import store from '../store';
-import { increment, decrement } from '../actions/action';
+import React, { Component } from "react";
+import store from "../store";
+import { increment, decrement } from "../actions/action";
 
 export default class Counter extends Component {
-	// Mounting
-	constructor(props) {
-		super(props);
+  // Mounting
+  constructor(props) {
+    super(props);
 
-		this.state = store.getState();
-		this.unsubscribe = store.subscribe(() => {
-			console.log(store.getState());
-			this.setState(store.getState());
-		});
-	}
+    this.state = store.getState();
+    this.unsubscribe = store.subscribe(() => {
+      console.log("Counter, subscribe()", store.getState());
+      this.setState(store.getState());
+    });
+  }
 
-	componentWillUnmount() {
-		super.componentWillUnmount();
+    componentWillUnmount() {
+      super.componentWillUnmount();
 
-		// Stop listening to state updates
-		this.unsubscribe();
-	}
+      // Stop listening to state updates
+      this.unsubscribe();
+    }
 
-	render() {
-		console.log('Counter,render():' + this.state);
-		return (
-			<div style={{ width: '50%' }}>
-				<p onClick={this._onPress4Increment}>+</p>
-				<p>{this.state.num}</p>
-				<p onClick={this._onPress4Decrement}>-</p>
-			</div>
-		);
-	}
-	_onPress4Increment() {
-		// Dispatching Actions
-		console.log('click increment');
-		store.dispatch(increment());
-	}
+  render() {
+    console.log("Counter,render():", this.state);
+    return (
+      <div style={{ width: "50%" }}>
+        <p onClick={this._onPress4Increment}>+</p>
+        <p>{this.state.num}</p>
+        <p onClick={this._onPress4Decrement}>-</p>
+      </div>
+    );
+  }
+  _onPress4Increment() {
+    // Dispatching Actions
+    console.log("click +");
+    store.dispatch(increment());
+  }
 
-	_onPress4Decrement() {
-		console.log('click decrement');
-		store.dispatch(decrement());
-	}
+  _onPress4Decrement() {
+    console.log("click -");
+    store.dispatch(decrement());
+  }
 }
--- Counter/src/reducers/counter.js
+++ Counter/src/reducers/counter.js
@@ -1,27 +1,42 @@
-import * as TYPES from '../constants';
+import * as TYPES from "../constants";
+
+var globalObj = {
+  num: 10
+};
 
 const defaultState = {
-	num: 0
+  num: 0
 };
 
 export default (state = defaultState, action) => {
-	console.log(state.num, action);
-	switch (action.type) {
-		case TYPES.INCREMENT:
-			// ES6
-			return Object.assign({}, state, {
-				num: state.num + 1
-			});
+  switch (action.type) {
+    case TYPES.INCREMENT:
+      // ES6
+      console.log("reducer: +");
+      return Object.assign({}, state, {
+        num: state.num + 1
+      });
+    // ES7
+    // case TYPES.DECREMENT:
+    //   return { ...state, num: state.num - 1 };
 
-		// ES7
-		// case TYPES.DECREMENT:
-		//     return { ...state, num: state.num - 1 };
+    //   return {
+    //     num: 10
+    //   };
 
-		case TYPES.DECREMENT:
-			// 将那些无需修改的项原封不动移入, 接着对需修改的项用新生成的对象替换。
-			return { ...state, ...{ num: state.num - 1 } };
+    //   console.log("reducer: +", globalObj);
+    //   return globalObj;
 
-		default:
-			return state;
-	}
+    case TYPES.DECREMENT:
+      // 将那些无需修改的项原封不动移入, 接着对需修改的项用新生成的对象替换。
+      console.log("reducer: +");
+      return { ...state, ...{ num: state.num - 1 } };
+    //   return {
+    //     num: 10
+    //   };
+    //   return globalObj;
+
+    default:
+      return state;
+  }
 };
